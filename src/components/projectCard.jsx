2a3b5b5d14db66f3819268757ab7edaf512ede55
Aziz Naufal <aziz13naufal@gmail.com>
2023-09-18 19:02:27 +0700

    membuat props url, repo, techStack

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react"
+
 export default function ProjectCard(props) {
     return (
-        <div className="bg-gradient-to-b from-[#f1eae4] to-[#e8e2dc] p-2 rounded-[5px] border border-[#EADFD7] cursor-pointer" id="project-card">
-            <div className="bg-[#f1eae4] h-full rounded-[3px] overflow-hidden text-[#654b36] w-72 relative">
+        <div className="bg-gradient-to-b from-[#f1eae4] to-[#e8e2dc] p-2 rounded-[5px] border border-[#EADFD7]" id="project-card">
+            <div className="bg-[#f1eae4] flex flex-col h-full rounded-[3px] overflow-hidden text-[#654b36] w-72 relative">
                 <div className="">
                     <img src={props.image} className='w-72' />
                 </div>
-                <div className="px-3 pt-2 pb-4">
+                <div className="px-3 pt-2 pb-4 flex flex-col flex-1">
                     <div className="font-semibold text-2xl">{props.title}</div>
                     <div className="font-normal text-base mt-2">{props.desc}</div>
                 </div>
-                <div className="hidden" id="card-icon">
-                    <div className="absolute top-0 right-0 mt-2 mr-2">😶 😯</div>
-                    <div className="absolute bottom-0 mb-2 ml-2">😂 😋</div>
+                <div className="flex items-end" id="card-icon">
+                    <div className=" bottom-0 mb-2 ml-2 flex justify-center h-[20px]">
+                        {props.techStack}
+                    </div>
+                    <div className="absolute flex top-0 right-0 mt-2 mr-2">
+                        <a href={props.repo} className={`cursor-pointer opacity-40 hover:opacity-100 ${props.repo === 'none' ? 'hidden' : ''}`} target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke={`${props.linkColor === 'black' ? 'black' : 'white'}`} class="w-6 h-6">
+                              <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75L16.5 12l-2.25 2.25m-4.5 0L7.5 12l2.25-2.25M6 20.25h12A2.25 2.25 0 0020.25 18V6A2.25 2.25 0 0018 3.75H6A2.25 2.25 0 003.75 6v12A2.25 2.25 0 006 20.25z" />
+                            </svg>
+                        </a>
+                        <a href={props.url} className={`cursor-pointer -mt-[2px] ml-1 opacity-40 hover:opacity-100 ${props.url === 'none' ? 'hidden' : ''}`} target="_blank">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke={`${props.linkColor === 'black' ? 'black' : 'white'}`} className="w-6 h-6">
+                              <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                            </svg>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/routes/projects.jsx
+++ src/routes/projects.jsx
@@ -4,6 +4,22 @@ import PortofolioWeb1 from "./../assets/img/web-portofolio-1.png"
 import MovieDB from "./../assets/img/movie-db.png"
 import MRTOPUP from "./../assets/img/MRTOPUP.png"
 
+import SkillCard from "../components/skillCard";
+import HTMLImage from "./../assets/logo/html.png";
+import CSSImage from "./../assets/logo/css.png";
+import JSImage from "./../assets/logo/js.png";
+import NodeJSImage from "./../assets/logo/nodejs.png";
+import NextJSImage from "./../assets/logo/nextjs.png";
+import ExpressImage from "./../assets/logo/expressjs.png";
+import ReactImage from "./../assets/logo/reactjs.png";
+import PrismaImage from "./../assets/logo/prisma.png";
+import TailwindImage from "./../assets/logo/tailwind.png";
+import BootstrapImage from "./../assets/logo/bootstrap.png";
+import PostgresqlImage from "./../assets/logo/postgresql.png";
+import MysqlImage from "./../assets/logo/mysql.png";
+import MongoDBlImage from "./../assets/logo/mongodb.png";
+import TypescriptImage from "./../assets/logo/typescript.png";
+
 export default function Projects() {
   return (
     <div className="bg-[#513c2c] flex flex-col items-center pb-10">
@@ -12,22 +28,57 @@ export default function Projects() {
         image={KiraWebsite}
         title="Kira Website"
         desc="This website is inspired by the Death Note anime, where people can write down a villain's name that can be seen by the public."
-        class="row-start-1 row-end-4" />
+        class="row-start-1 row-end-4"
+        techStack={
+          <>
+            <img src={TypescriptImage} className="mr-3" />
+            <img src={PrismaImage} className="mr-3" />
+            <img src={PostgresqlImage} className="mr-3" />
+            <img src={TailwindImage} className="mr-3 h-4" />
+          </>
+          }
+          url="https://kira-website.vercel.app/"
+          repo="https://github.com/aziz13naufal/kira-website" />
 
         <ProjectCard 
         image={MRTOPUP}
         title="MR TOP UP"
-        desc="Create a top up website interface, according to client request" />
+        desc="Create a top up website interface, according to client request"
+        techStack={
+          <>
+            <img src={CSSImage} className="mr-3" />
+            <img src={BootstrapImage} className="mr-3" />
+          </>
+          } 
+          url="https://mr-top-up.vercel.app/"
+          repo="none" />
         
         <ProjectCard
         image={PortofolioWeb1}
         title="Portofolio Website v1"
-        desc="My first portfolio website" />
+        desc="My first portfolio website"
+        techStack={
+          <>
+            <img src={TailwindImage} className="mr-3 h-4" />
+          </>
+          }
+        url="https://portofolio-web-v1.vercel.app/"
+        repo="https://github.com/aziz13naufal/portofolio-web-v1" />
 
         <ProjectCard
         image={MovieDB}
         title="Movie Database"
-        desc="Using the Fetch API to retrieve data from the OMDB API" />
+        desc="Using the Fetch API to retrieve data from the OMDB API"
+        techStack={
+          <>
+            <img src={JSImage} className="mr-3" />
+            <img src={BootstrapImage} className="mr-3" />
+          </>
+          } 
+        url="https://vercel.com/aziz13naufal/wpu-movie-db"
+        repo="https://github.com/aziz13naufal/WPU-movie-db"
+        linkColor="black" 
+        />
 
       </div>
     </div>
